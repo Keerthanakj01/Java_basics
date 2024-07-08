947d3f8263f18615dc490fe19e1627a09ecdd8c3
Keerthana K J <122909942+Keerthanakj01@users.noreply.github.com>
2024-07-08 11:42:53 +0530

    Add files via upload

---
+++ Loops/Simpleif.java
@@ -0,0 +1,11 @@
+public class Simpleif {
+    public static void main(String[] args){
+        int num=5;
+        if(num>0){
+            System.out.println("The number is positive");
+
+        }
+        
+    }
+    
+}
